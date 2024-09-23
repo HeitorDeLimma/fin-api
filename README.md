fd449f55e6289b78a04c5a8979fe1cf869cb7efb
HeitorDeLimma <heitordelima77@gmail.com>
2024-09-23 19:59:26 -0300

    Sexta-Commit

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
-# Fin API - Financeira
+# fin API = Financeira
 
-## Requisitos 
+## Requisitos
 
-- [] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um depósito
-- [] Deve ser possível realizar um saque
-- [] Deve ser possível realizar uma transferência via pix
+- [X] Deve ser possível criar uma conta
+- [X] Deve ser possível buscar o extrato bancário do cliente
+- [X] Deve ser possível realizar um depósito
+- [X] Deve ser possível realizar um saque
+- [] Deve ser possível realizar uma transferência via PIX
 - [] Deve ser possível buscar o extrato bancário do cliente por data
-- [] Deve ser possível atualizar os dados da conta do cliente
-- [] Deve ser possível obter os dados da conta do cliente
+- [] Deve ser possível atualizar dados da conta do cliente
+- [] Deve ser possível buscar balanço da conta do cliente
+- [X] Deve ser possível obter os dados da conta do cliente
 - [] Deve ser possível deletar uma conta
+- 
 
 ## Regras de negócio
 
-- [] Não deve ser possível cadastrar uma conta com CPF já existente
-- [] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar o extrato de uma conta não existente 
+- [X] Não deve ser possível cadastrar uma conta com CPF já existente
+- [X] Não deve ser possível fazer um depósito em uma conta não existente
+- [X] Não deve ser possível fazer um depósito menor que o depósito mínimo
+- [X] Não deve ser possível fazer um depósito/saque menor que o depósito mínimo
+- [] Não deve ser possível buscar o extrato em uma conta não existente
 - [] Não deve ser possível fazer um saque em uma conta não existente
 - [] Não deve ser possível fazer um pix de/para uma conta não existente
 - [] Não deve ser possível excluir uma conta não existente
 - [] Não deve ser possível fazer um saque quando o saldo for insuficiente
 - [] Não deve ser possível fazer um pix para si mesmo
-- [] Não deve ser possível fazer um pix quando o saldo for insuficiente
+- [] Não deve ser possível fazer um pix quand o saldo for insuficiente
 - [] Não deve ser possível fazer um pix acima do limite permitido
+- [] Não deve ser possível
---
+++ src/app.ts
@@ -0,0 +1,120 @@
+import { randomUUID } from 'crypto'
+import express from 'express'
+
+import { verifyExistsAccount } from './middlewares/verify-exists-account'
+import { Customer, Statement } from './types/customer'
+import { getBalance } from './utils/get-balance'
+
+const app = express()
+
+app.use(express.json())
+
+// Banco de dados em memória
+export const customers: Customer[] = []
+
+const minAmountOfDeposit = 0.1
+const minAmountOfWithdraw = 0.1
+
+// ROTA: Cria um novo cliente
+app.post('/account', (request, response) => {
+  // Capturando parâmetros do body
+  const { name, cpf } = request.body
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf,
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ message: 'Customer already exists' })
+  }
+
+  // Criando objeto do customer
+  const customer = {
+    id: randomUUID(),
+    name,
+    cpf,
+    statement: [],
+  }
+
+  // Adicionando customers a lista
+  customers.push(customer)
+
+  return response.status(201).json({
+    message: 'Customer created ',
+    data: customer,
+  })
+})
+
+// Rota: Busca os dados de um cliente
+app.get('/account/:cpf', verifyExistsAccount, (request, response) => {
+  const { customer } = request
+  return response.json({ data: customer })
+})
+
+// Rota: Busca o extrato de um cliente
+app.get('/statement/:cpf', verifyExistsAccount, (request, response) => {
+  const { customer } = request
+  return response.json({ data: customer.statement })
+})
+
+// Rota: Deposita um valor a um cliente
+app.post('/deposit/:cpf', verifyExistsAccount, (request, response) => {
+  const { customer } = request
+  const { description, amount } = request.body as {
+    description: string
+    amount: number
+  }
+
+  if (amount < minAmountOfDeposit) {
+    return response
+      .status(400)
+      .json({ message: 'Amount must be greater then the minimum amount' })
+  }
+
+  const statement: Statement = {
+    description,
+    amount,
+    type: 'credit',
+    createdAT: new Date(),
+  }
+
+  customer.statement.push(statement)
+
+  return response.json({
+    message: `Deposit worth ${amount} successfully made`,
+  })
+})
+
+// Rota: Saca um valor de um cliente
+app.post('/withdraw/:cpf', verifyExistsAccount, (request, response) => {
+  const { customer } = request
+  const { description, amount } = request.body as {
+    description: string
+    amount: number
+  }
+
+  if (amount < minAmountOfWithdraw) {
+    return response
+      .status(400)
+      .json({ message: 'Amount must be greater then the minimum amount' })
+  }
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return response.status(400).json({ message: 'Insufficient funds!' })
+  }
+
+  const statement: Statement = {
+    description,
+    amount,
+    type: 'debit',
+    createdAT: new Date(),
+  }
+
+  customer.statement.push(statement)
+
+  return response.json({ message: 'Successfully withdraw' })
+})
+
+export default app
